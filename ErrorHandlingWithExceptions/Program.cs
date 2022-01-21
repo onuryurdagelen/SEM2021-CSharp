1bcce8648b7d0ad80e6f7114d5ea6ee4c7349267
Onur Yurdagelen <68570364+onuryurdagelen@users.noreply.github.com>
2022-01-21 14:59:30 +0300

    Add files via upload

---
+++ ErrorHandlingWithExceptions/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ErrorHandlingWithExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int sayi1 = 1;
+                int sayi2 = 0;
+                int sonuc;
+                sonuc = sayi1 / sayi2;
+                Console.WriteLine(sonuc);
+            }
+            catch(FormatException error)
+            {
+                throw error;
+            }
+            catch (DivideByZeroException error)
+            {
+
+                throw error;
+            }
+            catch(System.Exception error)
+            {
+                throw error;
+            }
+            finally
+            {
+                Console.WriteLine("Finally kod blogu");
+            }
+        }
+    }
+}
